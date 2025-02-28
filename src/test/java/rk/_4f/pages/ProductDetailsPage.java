7437f5dc485e9f34a6644ad0c801947e4074874f
Radek Konfisz <rkonfisz@gmail.com>
2025-02-28 21:19:28 +0100

    RK refactor

--- src/test/java/rk/_4f/pages/ProductDetailsPage.java
+++ src/test/java/rk/_4f/pages/ProductDetailsPage.java
@@ -1,4 +1,18 @@
 package rk._4f.pages;
 
-public class ProductDetailsPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductDetailsPage extends BasePage<ProductDetailsPage>{
+
+    public ProductDetailsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy (css = "button[title=\"L\"]")
+    private WebElement buttonLSize;
+
+    @FindBy (css = "xpath = //div[contains(@class,'addToCartActions')]/button")
+    private WebElement buttonAddToCart;
 }
